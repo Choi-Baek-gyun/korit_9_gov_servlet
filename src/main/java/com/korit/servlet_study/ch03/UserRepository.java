2879c2b52174868771d019890537e05e14814963
chlqorrbs <qorrbs9341@gmail.com>
2025-11-04 16:22:54 +0900

    Servlet Filter

--- src/main/java/com/korit/servlet_study/ch03/UserRepository.java
+++ src/main/java/com/korit/servlet_study/ch03/UserRepository.java
@@ -3,11 +3,12 @@ package com.korit.servlet_study.ch03;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
+import java.util.Optional;
 
 public class UserRepository {
     private static UserRepository instance;
     private List<User> users;
-    private long autoId;
+    private long autoId = 0l;
     private UserRepository() {
         users = new ArrayList<>();
     }
@@ -25,8 +26,22 @@ public class UserRepository {
     }
 
     public User findByUsername(String username) {
-        users.stream().filter(user -> user.getUsername().equals(username))
-                .findFirst().get();
+        Optional<User> userOptional = users.stream()
+                .filter(user -> user.getUsername().equals(username))
+                .findFirst();
+
+        return userOptional.orElseGet(() -> null);
+
+    }
+    public User findByUsernameNonOptional(String username) {
+        List<User> foundUsers = users.stream()
+                .filter(user -> user.getUsername().equals(username))
+                .toList();
+        if (foundUsers.isEmpty()) {
+            return null;
+        }
+        return foundUsers.get(0);
+
     }
 
     public List<User> findAll() {
